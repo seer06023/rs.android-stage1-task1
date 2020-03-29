642d414ea745ad6a6dbf2c1e660af7b957b33a17
seer06023 <erseer@gmail.com>
2020-03-29 22:05:49 +0300

    feat: fourth task

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -2,8 +2,35 @@ package subtask4
 
 class StringParser {
 
-    // TODO: Complete the following function
-    fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+      fun closeBracket (typeBracket: Char): Char {
+        return if (typeBracket == '(') ')'
+        else if (typeBracket == '[') ']'
+        else if (typeBracket == '<') '>'
+        else ' '
     }
+}
+
+fun StringParser.getResult(inputString: String): Array<String> {
+    val output: MutableSet<String> = mutableSetOf()
+    inputString.indices.forEach { i ->
+        val firstBracket = inputString[i]
+        when (firstBracket) {
+            '(', '[', '<' -> {
+                val secondBracket = closeBracket(firstBracket)
+                var isClosed: Boolean = false
+                loop@ for (j in i + 1 until inputString.length) {
+                    if (inputString[j] == secondBracket) {
+                        output += inputString.substring(i+1, j)
+                        isClosed = true
+                    }
+                    when {
+                        inputString[j] == firstBracket && isClosed -> {
+                            break@loop
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return output.toTypedArray()
 }
--- src/test/kotlin/Task1StringParserTest.kt
+++ src/test/kotlin/Task1StringParserTest.kt
@@ -2,6 +2,7 @@ import org.junit.Test
 import org.junit.Assert.assertArrayEquals
 import org.junit.Assert.assertTrue
 import subtask4.StringParser
+import subtask4.getResult
 
 class Task1StringParserTest {
 
