ca1c5e8aea1849d39a4db347f7b403ac3d867125
seer06023 <erseer@gmail.com>
2020-03-29 22:19:48 +0300

    feat: fifth task

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -2,8 +2,40 @@ package subtask5
 
 class HighestPalindrome {
 
-    // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+        val output = digitString.toCharArray()
+        val endNumber = n - 1
+        var diff = 0
+        (0..endNumber / 2).forEach { i ->
+            when {
+                output[i] != output[endNumber-i] -> diff++
+            }
+            when {
+                diff >= k && i < (endNumber / 2) -> return "-1"
+            }
+        }
+        var count = k - diff
+        (0..endNumber / 2).forEach { i ->
+            when {
+                !(output[i] != output[endNumber - i] && count > 0) -> {
+                    when {
+                        output[i] != output[endNumber - i] -> {
+                            when {
+                                output[i] > output[endNumber - i] -> {
+                                    output[endNumber - i] = output[i]
+                                }
+                                else -> output[i] = output[endNumber - i]
+                            }
+                        }
+                    }
+                }
+                else -> {
+                    output[i] = '9'
+                    output[endNumber - i] = '9'
+                    count = count - 2
+                }
+            }
+        }
+        return String(output)
     }
 }
