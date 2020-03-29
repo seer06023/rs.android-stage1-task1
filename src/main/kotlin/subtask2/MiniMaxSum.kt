c2ed40ef75f0b1107056ca8703b968cf6886a1f2
seer06023 <erseer@gmail.com>
2020-03-29 21:29:31 +0300

    feat: second task

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -1,9 +1,32 @@
 package subtask2
 
+import kotlin.Int.Companion.MAX_VALUE
+
 class MiniMaxSum {
 
-    // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val outputArray = mutableListOf<Int>()
+        val tempArray: IntArray = IntArray(input.size)
+        var sumAll = 0
+        var max = 0
+        var min = 999999
+
+        for (i in 0 until input.size) sumAll += input[i]
+
+        (0..input.size - 1).forEach { i ->
+            tempArray[i] = sumAll - input[i]
+
+            max = if (tempArray[i] > max) {
+                tempArray[i]
+            } else max
+            min = if (tempArray[i] < min) {
+                tempArray[i]
+            } else min
+        }
+        outputArray.add(min)
+        outputArray.add(max)
+
+
+        return outputArray.toIntArray()
     }
 }
